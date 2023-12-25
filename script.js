027b6a7cdeaeab03f8f11f2a58e0995fc5257189
ahm3d777 <153632308+ahm3d777@users.noreply.github.com>
2023-12-25 17:21:20 +0600

    update
    12.25.2023

--- script.js
+++ script.js
@@ -27,3 +27,31 @@ document.addEventListener('scroll', function () {
         }
     });
 });
+
+// JavaScript for Back-to-Top Button
+document.addEventListener("DOMContentLoaded", function () {
+    var btn = document.getElementById("back-to-top-btn");
+
+    window.onscroll = function () {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            btn.style.display = "block";
+        } else {
+            btn.style.display = "none";
+        }
+    }
+
+    btn.addEventListener("click", function () {
+        topFunction();
+    });
+
+    function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+    }
+});
+
+
